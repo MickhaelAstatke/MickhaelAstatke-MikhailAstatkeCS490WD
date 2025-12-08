4dec7694e172cbd59230ebb3557f121009edb01b
MickhaelAstatke <155271538+MickhaelAstatke@users.noreply.github.com>
2025-12-07 23:02:08 -0600

    Update script.js

--- final/script.js
+++ final/script.js
@@ -40,23 +40,3 @@
   }
 })();
 
-// Scroll To Top Button
-(function initScrollToTop() {
-  const scrollBtn = document.getElementById("scrollTopBtn");
-
-  // Show button after scrolling 200px
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-      scrollBtn.style.display = "block";
-    } else {
-      scrollBtn.style.display = "none";
-    }
-  });
-\
-  scrollBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-})();
